6a160c6af051bff8960f518e9e716de8a02ebdaa
Hendrik <ky0n@users.noreply.github.com>
2018-06-09 14:40:59 +0200

    recycler view für highscore weiter

--- app/src/main/java/com/clickbubble/Adapter.kt
+++ app/src/main/java/com/clickbubble/Adapter.kt
@@ -1,26 +1,32 @@
 package com.clickbubble
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
 import com.clickbubble.model.Highscore
+import java.util.*
 
 /**
  * @author Hendrik
  */
-class Adapter(private val highscores: Array<Highscore>): RecyclerView.Adapter<Adapter.ViewHolder>() {
+class Adapter(private val highscores: ArrayList<Highscore>): RecyclerView.Adapter<Adapter.ViewHolder>() {
 
     class ViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // create a new view
+        val textView = LayoutInflater.from(parent.context)
+                .inflate(R.layout.view_highscore, parent, false) as TextView
+        return ViewHolder(textView)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.textView.text = java.lang.String.format(Locale.getDefault( ), "%s %d",
+                highscores[position].name, highscores[position].points )
     }
+
+    override fun getItemCount() = highscores.size
 }
--- app/src/main/java/com/clickbubble/HighscoreActivity.kt
+++ app/src/main/java/com/clickbubble/HighscoreActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import com.clickbubble.model.Highscore
 import kotlinx.android.synthetic.main.activity_highscore.*
 
 class HighscoreActivity : AppCompatActivity() {
@@ -16,7 +17,16 @@ class HighscoreActivity : AppCompatActivity() {
         setContentView(R.layout.activity_highscore)
 
         viewManager = LinearLayoutManager(this)
-        // viewAdapter = myAdapter(myDataset)
+
+        // creating sample data
+        val data: ArrayList<Highscore> = arrayListOf(
+                Highscore("Pexi", 43),
+                Highscore("Chicken", 122),
+                Highscore("Maroun", 343434),
+                Highscore(points = 5)
+        )
+
+        viewAdapter = Adapter(data)
 
         recyclerView = highscoreBoard.apply {
             // use this setting to improve performance if you know that changes
